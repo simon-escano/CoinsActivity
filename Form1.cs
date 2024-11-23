791a8b68c151119cff2a446c1406a0745cacf884
simonescano <escanosimonlyster@gmail.com>
2024-11-24 05:44:13 +0800

    Shortened code

--- Form1.cs
+++ Form1.cs
@@ -1,82 +1,71 @@
-using static CoinsActivity.Circles;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace CoinsActivity
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        public Form1() => InitializeComponent();
 
         private void btnChooseImage_Click(object sender, EventArgs e)
         {
-            using (OpenFileDialog dialog = new OpenFileDialog())
-            {
-                dialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;";
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    pbImage.Image = Image.FromFile(dialog.FileName);
-                }
-            }
+            using OpenFileDialog dialog = new OpenFileDialog { Filter = "Image Files|*.jpg;*.jpeg;*.png;" };
+            if (dialog.ShowDialog() == DialogResult.OK)
+                pbImage.Image = Image.FromFile(dialog.FileName);
         }
 
         private void btnCount_Click(object sender, EventArgs e)
         {
-            if (pbImage.Image == null)
-            {
-                MessageBox.Show("Please select an image first.");
-                return;
-            }
-
+            if (pbImage.Image == null) { MessageBox.Show("Please select an image first."); return; }
             pnResult.Enabled = true;
 
-            int c5 = 0, c10 = 0, c25 = 0, p1 = 0, p5 = 0, p10 = 0;
-            List<Circle> circles = Circles.Detect(pbImage);
+            var coins = new Coins(
+                new("c5", 0.05f), new("c10", 0.1f), new("c25", 0.25f),
+                new("p1", 1f), new("p5", 5f), new("p10", 10f)
+            );
 
-            foreach (var circle in circles)
+            int denomination = 0;
+            var circles = Circles.Detect(pbImage).OrderBy(c => c.Radius).ToList();
+
+            for (int i = 0; i < circles.Count; i++)
             {
-
-                if (circle.Radius <= 29.4)
-                {
-                    c5++;
-                }
-                else if (circle.Radius <= 32.3)
-                {
-                    c10++;
-                }
-                else if (circle.Radius <= 38.5)
-                {
-                    c25++;
-                }
-                else if (circle.Radius <= 45.1)
-                {
-                    p1++;
-                }
-                else if (circle.Radius <= 50.5)
-                {
-                    p5++;
-                }
-                else
-                {
-                    p10++;
-                }
+                if (i > 0 && Math.Abs(circles[i].Radius - circles[i - 1].Radius) >= 0.8)
+                    denomination++;
+                coins[denomination].count++;
             }
 
-            showTotal(c5, c10, c25, p1, p5, p10);
+            showTotal(coins);
         }
 
-        private void showTotal(int c5, int c10, int c25, int p1, int p5, int p10)
+        private void showTotal(Coins coins)
         {
-            tbC5.Text = c5.ToString();
-            tbC10.Text = c10.ToString();
-            tbC25.Text = c25.ToString();
-            tbP1.Text = p1.ToString();
-            tbP5.Text = p5.ToString();
-            tbP10.Text = p10.ToString();
-
-            tbTotal.Text = (c5 * 0.05 + c10 * 0.1 + c25 * 0.25 + p1 + p5 * 5 + p10 * 10).ToString();
+            tbC5.Text = coins["c5"].count.ToString();
+            tbC10.Text = coins["c10"].count.ToString();
+            tbC25.Text = coins["c25"].count.ToString();
+            tbP1.Text = coins["p1"].count.ToString();
+            tbP5.Text = coins["p5"].count.ToString();
+            tbP10.Text = coins["p10"].count.ToString();
+            tbTotal.Text = coins.GetTotal().ToString();
         }
+    }
+
+    public class Coins : List<Coin>
+    {
+        public Coins(params Coin[] coins) : base(coins) { }
+        public Coin this[string denomination] => this.FirstOrDefault(c => c.Denomination == denomination) ?? new(denomination, 0);
+        public float GetTotal() => this.Sum(c => c.Total);
+    }
+
+    public class Coin
+    {
+        public string Denomination { get; }
+        public float Amount { get; }
+        public int count;
+
+        public Coin(string denomination, float amount) => (Denomination, Amount) = (denomination, amount);
+        public float Total => Amount * count;
     }
 }
